82c260a3c4bcb1044fc68249202c31cb4c5d46ce
Andras Gerendas <andras.gerendas@gmail.com>
2023-04-21 23:03:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import { Login } from './Login'
+import { proxy } from './Proxy';
+import { Main } from './Main';
+
+export default class App extends Component
+{
+  state = { showLogin: true };
+
+  componentDidMount()
+  {
+    proxy.addEventListener( "login", () => this.setState( { showLogin: false } ) );
+  }
+
+  render()
+ {
+  return (
+    <div className="app">
+      { this.state.showLogin ? <Login /> : <Main /> }
+    </div>
+  );
+ }
+}
